65310282270b47953b6173db26d001d876cd080e
BetoMees <15331041+BetoMees@users.noreply.github.com>
2023-09-13 01:21:28 -0300

    add DeconstructTuples with switch expression

---
+++ CSharpLanguageFeatures/General/DeconstructTuples.cs
@@ -0,0 +1,30 @@
+﻿namespace CSharpLanguageFeatures.General
+{
+    class DeconstructTuples
+    {
+        private static (decimal, decimal, decimal) GetValues(string name) => name switch
+        {
+            "first" => (51, 45, 20),
+            "second" => (20, 10, 30),
+            "third" => (22, 47, 55),
+            "forth" or "last" => (17, 99, 45),
+            _ => (12, 42, 87)
+        };
+
+        public static void Main()
+        {
+            // access value individually using the ItenN syntax
+            var itemSyntax = GetValues("first");
+            Console.WriteLine($"{itemSyntax.Item1}, {itemSyntax.Item2}, {itemSyntax.Item3}");
+
+            // deconstructing the turples into variables 
+            var (descVal1, descVal2, descVal3) = GetValues("second");
+            Console.WriteLine($"{descVal1}, {descVal2}, {descVal3}");
+
+            // ignoring some values
+            var (_, _, notIgnored) = GetValues("last");
+            Console.WriteLine($"{notIgnored}");
+
+        }
+    }
+}
