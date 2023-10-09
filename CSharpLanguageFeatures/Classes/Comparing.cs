1ea53f3c2c0d760d6380a3bb79a22d6d2ff6dc9d
BetoMees <15331041+BetoMees@users.noreply.github.com>
2023-10-08 21:33:04 -0300

    Introduce Class Comparing and Record Improve the Menu code

---
+++ CSharpLanguageFeatures/Classes/Comparing.cs
@@ -0,0 +1,84 @@
+﻿using CSharpLanguageFeatures.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLanguageFeatures.Classes
+{
+    [Show]
+    internal class Comparing
+    {
+        public static void Main()
+        {
+            Point2D p1 = new Point2D() { X = 10, Y = 20 };
+            Point2D p2 = new Point2D() { X = 10, Y = 20 };
+            Point2D p3 = null, p4 = null;
+
+            Console.WriteLine($"----Override----");
+            Console.WriteLine($"{p1} Equals {p2}"); // Point2D X:10 Y:20 Equals Point2D X:10 Y:20
+            Console.WriteLine($"{p1.Equals(p2)}");  // True
+
+            Console.WriteLine($"{p1} == {p2}");     // Point2D X:10 Y:20 == Point2D X:10 Y:20
+            Console.WriteLine($"{p1 == p2}");       // True
+
+            Console.WriteLine($"{p1} == {p2}");     // Point2D X:10 Y:20 == Point2D X:10 Y:20
+            Console.WriteLine($"{p1 != p2}");       // False
+
+            Console.WriteLine($"{p3} == {p4}");     //  ==
+            Console.WriteLine($"{p3 == p4}");       // True
+
+
+            Point2DRecord recordP1 = new Point2DRecord(X: 10, Y: 20) { };
+            Point2DRecord recordP2 = new Point2DRecord(X: 10, Y: 20) { };
+            Point2DRecord recordP3 = null, recordP4 = null;
+
+            Console.WriteLine($"----Record----");
+            Console.WriteLine($"{recordP1} Equals {recordP2}");     // Point2DRecord { X = 10, Y = 20 } Equals Point2DRecord { X = 10, Y = 20 }
+            Console.WriteLine($"{recordP1.Equals(recordP2)}");      // True
+
+            Console.WriteLine($"{recordP1} == {recordP2}");         // Point2DRecord { X = 10, Y = 20 } == Point2DRecord { X = 10, Y = 20 }
+            Console.WriteLine($"{recordP1 == recordP2}");           // True
+
+            Console.WriteLine($"{recordP1} != {recordP2}");         // Point2DRecord { X = 10, Y = 20 } != Point2DRecord { X = 10, Y = 20 }
+            Console.WriteLine($"{recordP1 != recordP2}");           // False
+
+            Console.WriteLine($"{recordP3} == {recordP4}");         //  ==
+            Console.WriteLine($"{recordP3 == recordP4}");           // True
+
+            Point2DRecord recordP5 = recordP2 with { Y = 30 };
+            Console.WriteLine($"{recordP1} == {recordP5}");         // Point2DRecord { X = 10, Y = 20 } == Point2DRecord { X = 10, Y = 30 }
+            Console.WriteLine($"{recordP1 == recordP5}");           // False
+
+        }
+        public record Point2DRecord(int X, int Y);
+        public class Point2D : IEquatable<Point2D>
+        {
+            public int X;
+            public int Y;
+            public override bool Equals(object? obj) => Equals(obj as Point2D);
+            public bool Equals(Point2D? p) => p != null && (X == p.X && Y == p.Y);
+            public override int GetHashCode() => (X, Y).GetHashCode();
+            public static bool operator ==(Point2D? p1, Point2D? p2)
+            {
+                if (p1 is null)
+                {
+                    if (p2 is null)
+                    {
+                        return true;
+                    }
+                    return false;
+                }
+                return p1.Equals(p2);
+            }
+
+            public static bool operator !=(Point2D? p1, Point2D? p2) => !(p1 == p2);
+
+            public override string ToString()
+            {
+                return $"{GetType().Name} X:{X} Y:{Y}";
+            }
+        }
+    }
+}
---
+++ CSharpLanguageFeatures/Utils/Menu.cs
@@ -0,0 +1,111 @@
+﻿using CSharpLanguageFeatures.Utils;
+using System.Reflection;
+
+
+public class Menu
+{
+    const int SECTION_SPACE = 50;
+    int _pos = 0;
+    List<Type> listExamples;
+    int Length { get => listExamples.Count - 1; }
+    public Menu()
+    {
+        listExamples = GetExampleTypes();
+
+        if (listExamples.Count == 0)
+        {
+            Console.WriteLine("Sorry couldn't find any classes in this project");
+            return;
+        }
+
+        RunExampleSelection();
+    }
+
+    List<Type> GetExampleTypes()
+    {
+        AssemblyName? assemblyName = Assembly.GetEntryAssembly()?.GetName();
+        if (assemblyName == null)
+        {
+            Console.WriteLine("Error: Unable to retrieve the entry assembly.");
+            return new List<Type>();
+        }
+
+        return AppDomain.CurrentDomain.GetAssemblies()
+            .Where(assembly => assembly.FullName == assemblyName.FullName)
+            .SelectMany(assembly => assembly.GetTypes()
+                .Where(type => type.FullName?.StartsWith($"{assemblyName.Name}.") ?? false)
+                .Where(type => type.IsDefined(typeof(ShowAttribute), false))
+            )
+            .OrderBy(type => type.FullName)
+            .ToList();
+    }
+    void RunExampleSelection()
+    {
+        PrintExampleSelection(listExamples, _pos);
+        ConsoleKeyInfo outputKey = Console.ReadKey();
+        if (outputKey.Key == ConsoleKey.Enter)
+        {
+            ExecuteExample(listExamples[_pos]);
+            RunExampleSelection();
+        }
+        else if(outputKey.Key != ConsoleKey.Escape)
+        {
+            _pos = MovePosition(outputKey.Key, _pos);
+            RunExampleSelection();
+        }
+    }
+
+    void PrintExampleSelection(List<Type> listExamples, int pos)
+    {
+        Console.Clear();
+        Console.WriteLine("Select one of the examples to execute or Esc to leave:");
+        string section = "";
+
+        foreach (var (type, i) in listExamples.Select((value, i) => (value, i)))
+        {
+            if (type.FullName?.Split(".")[1..^1] is string[] splited && splited.Length > 0)
+            {
+                var joinSection = String.Join(" ", splited);
+                if (!joinSection.Equals(section))
+                {
+                    section = joinSection;
+                    string strSpace = new string('-', (SECTION_SPACE - joinSection.Length) / 2);
+                    Console.WriteLine($"{strSpace + joinSection + strSpace}");
+                }
+            }
+            Console.WriteLine($"{(pos == i ? "=>" : "  ")} {type.Name}");
+        }
+    }
+
+    void ExecuteExample(Type type)
+    {
+        Console.Clear();
+        try
+        {
+            MethodInfo? mainMethod = type.GetMethod("Main");
+            if (mainMethod != null && mainMethod.IsStatic)
+            {
+                mainMethod.Invoke(null, null);
+            }
+            else
+            {
+                Console.WriteLine("Please ensure that the Main method is both implemented and declared as public and static.");
+            }
+        }
+        catch (ArgumentOutOfRangeException)
+        {
+            Console.WriteLine("Please select a valid option");
+        }
+        Console.WriteLine("Press any key to return");
+        _ = Console.ReadKey();
+    }
+    int MovePosition(ConsoleKey key, int pos)
+    {
+        return key switch
+        {
+            ConsoleKey.UpArrow => --pos >= 0 ? pos : Length,
+            ConsoleKey.DownArrow => ++pos <= Length ? pos : 0,
+            _ => pos
+        };
+    }
+}
